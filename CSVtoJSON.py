0d3d3e3cb544d6561b1eeb9e8f9d72528103b9e6
yandax <48220823+yandax@users.noreply.github.com>
2020-06-24 15:59:51 -0400

    Added country-specific data

--- CSVtoJSON.py
+++ CSVtoJSON.py
@@ -10,6 +10,7 @@ content = requests.get(url).content
 csv = pd.read_csv(io.StringIO(content.decode('utf-8')))
 
 conflist = []
+country = {}
 
 for i in range(len(csv.index)):
     mname = str(csv["Admin2"][i])
@@ -31,11 +32,75 @@ for i in range(len(csv.index)):
     rate = str(csv["Incidence_Rate"][i])
     cfr = str(csv["Case-Fatality_Ratio"][i])
     conflist.append({"mname": mname, "pname": pname, "cname": cname, "lat": lat, "lon": lon, "conf": conf, "dead": dead, "recv": recv, "active": active, "name": name, "rate": rate, "cfr": cfr})
+    if cname not in country:
+        if lat == "":
+            country[cname] = {"name": cname, "lat": 0, "lon": 0, "conf": conf, "dead": dead, "recv": recv, "active": active, "num": 0}
+        else:
+            country[cname] = {"name": cname, "lat": lat, "lon": lon, "conf": conf, "dead": dead, "recv": recv, "active": active, "num": 1}
+    else:
+        if lat == "":
+            country[cname] = {"name": cname, "lat": country[cname]["lat"], "lon": country[cname]["lon"],
+                              "conf": country[cname]["conf"] + conf, "dead": country[cname]["dead"] + dead,
+                              "recv": country[cname]["recv"] + recv, "active": country[cname]["active"] + active,
+                              "num": country[cname]["num"]}
+        else:
+            country[cname] = {"name": cname, "lat": country[cname]["lat"] + lat, "lon": country[cname]["lon"] + lon,
+                              "conf": country[cname]["conf"] + conf, "dead": country[cname]["dead"] + dead,
+                              "recv": country[cname]["recv"] + recv, "active": country[cname]["active"] + active,
+                              "num": country[cname]["num"] + 1}
+
+country["US"]["lat"] = 39.8283
+country["US"]["lon"] = -98.5795
+country["US"]["num"] = 1
+
+country["Canada"]["lat"] = 56.1304
+country["Canada"]["lon"] = -106.3468
+country["Canada"]["num"] = 1
+
+country["United Kingdom"]["lat"] = 55.3781
+country["United Kingdom"]["lon"] = -3.4360
+country["United Kingdom"]["num"] = 1
+
+country["Netherlands"]["lat"] = 52.1326
+country["Netherlands"]["lon"] = 5.2913
+country["Netherlands"]["num"] = 1
+
+country["France"]["lat"] = 46.2276
+country["France"]["lon"] = 2.2137
+country["France"]["num"] = 1
+
+country["Denmark"]["lat"] = 56.2639
+country["Denmark"]["lon"] = 9.5018
+country["Denmark"]["num"] = 1
+
+country["China"]["lat"] = 35.8617
+country["China"]["lon"] = 104.1954
+country["China"]["num"] = 1
+
+country["Russia"]["lat"] = 61.5240
+country["Russia"]["lon"] = 105.3188
+country["Russia"]["num"] = 1
+
+country["India"]["lat"] = 20.5937
+country["India"]["lon"] = 78.9629
+country["India"]["num"] = 1
+
+countrylist = []
+for i in country:
+    if country[i]["num"] != 0:
+        country[i]["lat"] /= country[i]["num"]
+        country[i]["lon"] /= country[i]["num"]
+    countrylist += [country[i]]
 
 conflist.sort(key=lambda s: s['conf'], reverse=True)
+countrylist.sort(key=lambda s: s['conf'], reverse=True)
 
 fileout = open(today+".js", "w")
 fileout.write("data = " + str(conflist))
+fileout.close()
+
+fileout = open(today+"-country.js", "w")
+fileout.write("data = " + str(countrylist))
 fileout.close()
 
 '''
